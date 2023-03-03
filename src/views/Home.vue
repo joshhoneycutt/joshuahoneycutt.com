af2560f76e582680993596456872a22bbabc561c
Joshua Honeycutt <joshdhh@gmail.com>
2023-03-03 14:42:38 -0500

    adding vuetifyjs

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <!--<HelloWorld />-->
+  <div class="timelne">
+    <Timeline />
+  </div>
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import Timeline from '@/components/Timeline.vue';
+</script>
+
+<style>
+  .timelne {
+    width: 50%;
+    margin: 0 auto;
+  }
+</style>
