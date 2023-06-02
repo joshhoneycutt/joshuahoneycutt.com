72dcd11602f063f5eb9d80b1de9a855fca0d79c6
Joshua Honeycutt <joshdhh@gmail.com>
2023-06-01 23:00:08 -0400

    edit style and logos

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,27 +1,9 @@
 <template>
     <v-container>
         <v-row justify="center">
-            <v-col cols="auto">
-                <v-sheet class="pa-2 ma-2">
-                    Web Technologies
-                    <v-row no-gutters>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                <v-img
-                                :min-width="25"
-                                width="25"
-                                aspect-ratio="16/9"
-                                cover
-                                src="@/assets/html_logo.png"
-                            ></v-img>
-                            </v-sheet>
-                        </v-col>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                HTML
-                            </v-sheet>
-                        </v-col>
-                    </v-row>
+            <v-sheet class="pa-2 ma-2">
+                Web Technologies
+                <template v-for="item in webTechnologies">
                     <v-row no-gutters>
                         <v-col cols="auto">
                             <v-sheet class="pa-2 ma-2">
@@ -30,16 +12,21 @@
                                     width="25"
                                     aspect-ratio="16/9"
                                     cover
-                                    src="@/assets/css_logo.png"
+                                    :src="item.photo"
                                 ></v-img>
                             </v-sheet>
                         </v-col>
                         <v-col cols="auto">
                             <v-sheet class="pa-2 ma-2">
-                                CSS
+                                {{ item.name }}
                             </v-sheet>
                         </v-col>
                     </v-row>
+                </template>
+            </v-sheet>
+            <v-sheet class="pa-2 ma-2">
+                Languages
+                <template v-for="item in languages">
                     <v-row no-gutters>
                         <v-col cols="auto">
                             <v-sheet class="pa-2 ma-2">
@@ -48,57 +35,21 @@
                                     width="25"
                                     aspect-ratio="16/9"
                                     cover
-                                    src="@/assets/vuejs_logo.png"
+                                    :src="item.photo"
                                 ></v-img>
                             </v-sheet>
                         </v-col>
                         <v-col cols="auto">
                             <v-sheet class="pa-2 ma-2">
-                                VueJS
+                                {{ item.name }}
                             </v-sheet>
                         </v-col>
                     </v-row>
-                </v-sheet>
-            </v-col>
-            <v-col cols="auto">
-                <v-sheet class="pa-2 ma-2">
-                    Languages
-                    <v-row no-gutters>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                <v-img
-                                :min-width="25"
-                                width="25"
-                                aspect-ratio="16/9"
-                                cover
-                                src="@/assets/js_logo.png"
-                                ></v-img>
-                            </v-sheet>
-                        </v-col>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                Javascript
-                            </v-sheet>
-                        </v-col>
-                    </v-row>
-                    <v-row no-gutters>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                <v-img class="image-adjustment"
-                                    :min-width="25"
-                                    width="25"
-                                    aspect-ratio="16/9"
-                                    cover
-                                    src="@/assets/java_logo.png"
-                                ></v-img>
-                            </v-sheet>
-                        </v-col>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                Java
-                            </v-sheet>
-                        </v-col>
-                    </v-row>
+                </template>
+            </v-sheet>
+            <v-sheet class="pa-2 ma-2">
+                Cloud / Databases
+                <template v-for="item in cloudAndDatabases">
                     <v-row no-gutters>
                         <v-col cols="auto">
                             <v-sheet class="pa-2 ma-2">
@@ -107,95 +58,18 @@
                                     width="25"
                                     aspect-ratio="16/9"
                                     cover
-                                    src="@/assets/python_logo.png"
+                                    :src="item.photo"
                                 ></v-img>
                             </v-sheet>
                         </v-col>
                         <v-col cols="auto">
                             <v-sheet class="pa-2 ma-2">
-                                Python
+                                {{ item.name }}
                             </v-sheet>
                         </v-col>
                     </v-row>
-                    <v-row no-gutters>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                <v-img
-                                    :min-width="25"
-                                    width="25"
-                                    aspect-ratio="16/9"
-                                    cover
-                                    src="@/assets/golang_logo.png"
-                                ></v-img>
-                            </v-sheet>
-                        </v-col>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                Golang
-                            </v-sheet>
-                        </v-col>
-                    </v-row>
-                    <v-row no-gutters>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                <v-img
-                                    :min-width="25"
-                                    width="25"
-                                    aspect-ratio="16/9"
-                                    cover
-                                    src="@/assets/csharp_logo.png"
-                                ></v-img>
-                            </v-sheet>
-                        </v-col>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                C#
-                            </v-sheet>
-                        </v-col>
-                    </v-row>
-                </v-sheet>
-            </v-col>
-            <v-col cols="auto">
-                <v-sheet class="pa-2 ma-2">
-                    Cloud / Databases
-                    <v-row no-gutters>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                <v-img
-                                    :min-width="25"
-                                    width="25"
-                                    aspect-ratio="16/9"
-                                    cover
-                                    src="@/assets/aws_logo.png"
-                                ></v-img>
-                            </v-sheet>
-                        </v-col>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                AWS
-                            </v-sheet>
-                        </v-col>
-                    </v-row>
-                    <v-row no-gutters>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                <v-img
-                                    :min-width="25"
-                                    width="25"
-                                    aspect-ratio="16/9"
-                                    cover
-                                    src="@/assets/salesforce_logo.png"
-                                ></v-img>
-                            </v-sheet>
-                        </v-col>
-                        <v-col cols="auto">
-                            <v-sheet class="pa-2 ma-2">
-                                Salesforce
-                            </v-sheet>
-                        </v-col>
-                    </v-row>
-                </v-sheet>
-            </v-col>
+                </template>
+            </v-sheet>
         </v-row>
     </v-container>
 </template>
@@ -205,15 +79,48 @@
     data: () => ({
         webTechnologies: [
         {
-            name: '',
-            photo: ''
+            name: 'HTML',
+            photo: 'html_logo.png'
+        },
+        {
+            name: 'CSS',
+            photo: 'css_logo.png'
+        },
+        {
+            name: 'VueJS',
+            photo: 'vuejs_logo.png'
         },
         ],
-        other: [
+        languages: [
         {
-            avatar: '',
-            title: '',
-            link: ''
+            name: 'Javascript',
+            photo: 'js_logo.png'
+        },
+        {
+            name: 'Java',
+            photo: 'java_logo.png'
+        },
+        {
+            name: 'Python',
+            photo: 'python_logo.png'
+        },
+        {
+            name: 'Golang',
+            photo: 'golang_logo.png'
+        },
+        {
+            name: 'C#',
+            photo: 'csharp_logo.png'
+        },
+        ],
+        cloudAndDatabases: [
+        {
+            name: 'AWS',
+            photo: 'aws_logo.png'
+        },
+        {
+            name: 'Salesforce',
+            photo: 'salesforce_logo.png'
         },
         ],
   })
